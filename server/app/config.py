838cf092810dade41c8ac5780c17f52afa6eedb5
Amanmeena0 <meenaaman581@gmail.com>
2026-05-25 21:43:39 +0530

    Refactor database configuration: Remove default SQLite URI and clean up unused files

--- server/app/config.py
+++ server/app/config.py
@@ -5,6 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'aman-meena')
-    # Default to SQLite if no DATABASE_URL is provided
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///crime.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
