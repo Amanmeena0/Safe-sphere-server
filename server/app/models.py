7eb1ebdc99e133e2df16aa1fbe3207f70ecff06a
Amanmeena0 <meenaaman581@gmail.com>
2026-06-02 17:03:35 +0530

    refactor: Remove unused hello_auth route and clean up model UUID function

--- server/app/models.py
+++ server/app/models.py
@@ -2,12 +2,8 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from uuid import uuid4
 
 db = SQLAlchemy()
-
-def get_uuid():
-    return uuid4().hex
 
 class User(db.Model):
     __tablename__ = 'users'
--- server/run.py
+++ server/run.py
@@ -1,13 +1,6 @@
-# run.py
-import os
-import sys
 from app import create_app
 
-# Add the project directory to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '')))
-
 app = create_app()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
