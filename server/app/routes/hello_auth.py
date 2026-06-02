adeea31a5197fb804de6479d9edf44743d7da7b1
Amanmeena0 <meenaaman581@gmail.com>
2026-06-02 17:05:12 +0530

    feat: Update hello_world message to reflect Safe-sphere branding

--- server/app/routes/hello_auth.py
+++ server/app/routes/hello_auth.py
@@ -4,4 +4,4 @@ hello_bp = Blueprint('hello', __name__)
 
 @hello_bp.route('/', methods=['GET'])
 def hello_world():
-    return jsonify(message="Hello, World!")
+    return jsonify(message="Hi, Welcome to Safe-sphere Backend System")
