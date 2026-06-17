ebcdcb55cf524adea70b9a785450f2388dbc001e
Amanmeena0 <meenaaman581@gmail.com>
2026-06-17 15:19:24 +0530

    feat: configure SQLAlchemy engine with pool settings for improved performance

--- app/models/database.py
+++ app/models/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.sqlalchemy_database_url)
+engine = create_engine(
+    settings.sqlalchemy_database_url,
+    pool_pre_ping=True,
+    pool_recycle=3600
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base() 
   
